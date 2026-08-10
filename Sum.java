03436aed5cba5e98236c45f028266aca30887e17
mounika <mounika9900@gmail.com>
2026-08-10 02:38:06 -0700

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum {
     public static void main(String [] argm){
         int a = 5;
         int b = 3;
-        System.out.println(a+b);
+        System.out.println("The sum is " + (a+b));
     }
 }
